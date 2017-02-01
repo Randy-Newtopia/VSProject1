2ebe04d6e49521dd5dfea8a8d0517a7f39566bf5
Randy-Newtopia <rvillanueva@newtopia.com>
2017-01-31 19:21:36 -0500

    Bind to Json

--- LandingPage1/LandingPage1/DailyGoals.aspx.vb
+++ LandingPage1/LandingPage1/DailyGoals.aspx.vb
@@ -1,60 +1,183 @@
 ﻿'Source credits:: https://codyhouse.co/demo/horizontal-timeline/index.html
-
+Imports System
 Imports Newtonsoft.Json
-'Public Class DailyGoal
+Imports Newtonsoft.Json.Linq
 
-'    Public Property DayLabel As String
-'    Public Property DateValue As DateTime
+Imports System.IO
 
-'End Class
+
+Namespace Global.NewtopiaDailyGoals
+
+    Public Class Comments
+        Public text As String
+    End Class
+
+    Public Class Goal
+        Public type As Integer
+        Public title As String
+        Public status As Integer
+        Public goal_id As String
+        Public comments As Comments
+    End Class
+
+    Public Class dailyschedule
+        Public schedule As String
+        Public status As Integer
+        Public goals As List(Of Goal)
+    End Class
+
+    Public Class Payload
+        Public status As String
+        Public dailyschedules As list(Of dailyschedule)
+    End Class
+
+End Namespace
+
+
 
 Public Class DailyGoals
     Inherits System.Web.UI.Page
 
-    'Private Sub GetDailyGoals(ByVal jsonDailyGoalObj As String)
-    '    Dim startDate As DateTime = DateTime.Today.AddDays(-21)
-    '    Dim endDate As DateTime = DateTime.Today.AddDays(7)
-    '    Dim dates As IEnumerable(Of DateTime) = Enumerable.Range(0, 1 + CInt((endDate - startDate).TotalDays)).Select(Function(n) startDate.AddDays(n)).ToArray()
-    '    Const ListItemTemplate As String = "<li><a href='#0' data-date='{0}' Class='{1}'>{2}</a></li>"
+    'Test Payload1
+    Private Function GetDailyGoals() As String
+        Return "
+            {
+              'status': 'success',
+              'dailyschedules': [
+                {
+                  'schedule': '2017-01-18',
+                  'status': 0,
+                  'goals': [
+                    {
+                      'type': 1,
+                      'title': 'Eat Breakfast',
+                      'status': 0,
+                      'goal_id': 'a0sg0000002cJyIAAU',
+                      'comments': {
+                        'text': 'Take Breakfast Rationale'
+                      }
+                    }
+                  ]
+                },
+                {
+                  'schedule': '2017-01-19',
+                  'status': 0,
+                  'goals': [
+                    {
+                      'type': 1,
+                      'title': 'Eat Breakfast',
+                      'status': 0,
+                      'goal_id': 'a0sg0000002cJyIAAU',
+                      'comments': {
+                        'text': 'Take Breakfast Rationale'
+                      }
+                    }
+                  ]
+                },
+                {
+                  'schedule': '2017-01-28',
+                  'status': 0,
+                  'goals': [
+                    {
+                      'type': 1,
+                      'title': 'Eat Breakfast',
+                      'status': 0,
+                      'goal_id': 'a0sg0000002cJy3AAE',
+                      'comments': {
+                        'text': 'Take Breakfast Rationale'
+                      }
+                    }
+                  ]
+                },
+                {
+                  'schedule': '2017-01-29',
+                  'status': 0,
+                  'goals': [
+                    {
+                      'type': 1,
+                      'title': 'Eat Breakfast',
+                      'status': 0,
+                      'goal_id': 'a0sg0000002cJy3AAE',
+                      'comments': {
+                        'text': 'Take Breakfast Rationale'
+                      }
+                    }
+                  ]
+                },
+                {
+                  'schedule': '2017-02-01',
+                  'status': 0,
+                  'goals': [
+                    {
+                      'type': 1,
+                      'title': 'Eat Breakfast',
+                      'status': 0,
+                      'goal_id': 'a0sg0000002cJyDAAU',
+                      'comments': {
+                        'text': 'Take Breakfast Rationale'
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+        "
+    End Function
 
-    '    For Each d As DateTime In dates
-    '        If d = Date.Today Then
-    '            oTimeLine.InnerHtml += String.Format(ListItemTemplate, d.ToString("dd/MM/yyyy"), "selected", d.ToString("dd MMM"))
-    '        ElseIf d > Date.Today Then
-    '            oTimeLine.InnerHtml += String.Format(ListItemTemplate, d.ToString("dd/MM/yyyy"), "disabled", d.ToString("dd MMM"))
-    '        Else
-    '            oTimeLine.InnerHtml += String.Format(ListItemTemplate, d.ToString("dd/MM/yyyy"), String.Empty, d.ToString("dd MMM"))
-    '        End If
-    '    Next
-    'End Sub
+    'Test Payload From File
+    Private Function GetDailyGoals(ByVal Filepath As String) As String
+        Dim jsonStr As String = File.ReadAllText(Filepath)
 
-    Private Sub InitGoalTimeLine()
+        Return jsonStr
+    End Function
+
+    Private Sub InitGoalTimeLine(ByVal jsonStr As String)
         Dim startDate As DateTime = DateTime.Today.AddDays(-20)
         Dim endDate As DateTime = DateTime.Today.AddDays(7)
 
-        Dim dates As IEnumerable(Of DateTime) = Enumerable.Range(0, 1 + CInt((endDate - startDate).TotalDays)).Select(Function(n) startDate.AddDays(n)).ToArray()
-        Const oTimeLineListItemTemplate As String = "<li><a href='#0' data-date='{0}' Class='{1}'>{2}</a></li>"
-        Const oScrollContentListItemTemplate As String = "<li Class='{0}' data-date='{1}'><div><h2>{2}</h2><em>{3}</em><p>{4}</p></div></li>"
+        Try
+            Dim x As NewtopiaDailyGoals.Payload = JsonConvert.DeserializeObject(Of NewtopiaDailyGoals.Payload)(GetDailyGoals()) ' Deserialize array of Post objects
 
+            Dim dates As IEnumerable(Of DateTime) = Enumerable.Range(0, 1 + CInt((endDate - startDate).TotalDays)).Select(Function(n) startDate.AddDays(n)).ToArray()
+            Const oTimeLineListItemTemplate As String = "<li><a href='#0' data-date='{0}' Class='{1}'>{2}</a></li>"
+            Const oScrollContentListItemTemplate As String = "<li Class='{0}' data-date='{1}'><div><h2>{2}</h2><em>{3}</em><p>{4}<br/>{5}</p></div></li>"
 
-        For Each d As DateTime In dates
-            Dim listClass As String = String.Empty
-            If d = Date.Today Then
-                listClass = "selected"
-            ElseIf d > Date.Today Then
-                listClass = "disabled"
-            Else
-                listClass = String.Empty
-            End If
-            oTimeLine.InnerHtml += String.Format(oTimeLineListItemTemplate, d.ToString("dd/MM/yyyy"), listClass, d.ToString("dd MMM"))
-            oScrollContent.InnerHtml += String.Format(oScrollContentListItemTemplate, listClass, d.ToString("dd/MM/yyyy"), "Goals", d.ToString("dd MMM"), "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.")
-        Next
+            Dim listClass As String = "selected"
+            For Each y As NewtopiaDailyGoals.dailyschedule In x.dailyschedules
+                oTimeLine.InnerHtml += String.Format(oTimeLineListItemTemplate, Date.Parse(y.schedule).ToString("dd/MM/yyyy"), listClass, Date.Parse(y.schedule).ToString("dd MMM"))
+                oScrollContent.InnerHtml += String.Format(oScrollContentListItemTemplate, listClass, Date.Parse(y.schedule).ToString("dd/MM/yyyy"), "Goals", Date.Parse(y.schedule).ToString("dd MMM"), y.goals(0).title, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.")
+                If listClass = "selected" Then
+                    listClass = ""
+                End If
+            Next
+            'For Each d As DateTime In dates
+            '    Dim listClass As String = String.Empty
+            '    If d = Date.Today Then
+            '        listClass = "selected"
+            '    ElseIf d > Date.Today Then
+            '        listClass = "disabled"
+            '    Else
+            '        listClass = String.Empty
+            '    End If
+            '    oTimeLine.InnerHtml += String.Format(oTimeLineListItemTemplate, d.ToString("dd/MM/yyyy"), listClass, d.ToString("dd MMM"))
+            '    oScrollContent.InnerHtml += String.Format(oScrollContentListItemTemplate, listClass, d.ToString("dd/MM/yyyy"), "Goals", d.ToString("dd MMM"), "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.")
+            'Next
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
 
     End Sub
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
-            InitGoalTimeLine()
+            'Dim jSonStr As String = GetJSonStr("\JsonObj\Goals_Payload.txt")
+            Dim jSonStr As String = GetDailyGoals()
+            'Dim json As String = File.ReadAllText("JSON File Path")
+            'Dim o As JObject = JObject.Parse(json)
+
+            InitGoalTimeLine(jSonStr)
+
         End If
     End Sub
 
